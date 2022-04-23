345206e751441af063517794d9179f36a6cd1016
kiran-goswami <kiran191541535@gmail.com>
2022-04-23 15:18:19 +0530

    v1

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("data types:-we are learning about primitive data types")
+console.log("1.boolean , 2.string , 3.numbers")
+console.log("boolean data type contains only two statements true or false ")
+//  example for let
+let fName;
+fName = "kiran";
+console.log(fName);
+let lName = "goswami";
+console.log(lName);
+// example for  var
+var age = 19;
+var age;
+age = 19;
+console.log(age);
+// example of const
+const num1= "10";
+console.log("10");
+
